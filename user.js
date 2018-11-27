9f5b831cbd4ba1e9eff4bd2aeed5455d90d20c37
Larrygf02 <raulgf_02@hotmail.com>
2018-11-27 00:55:54 -0500

    API login usuario y create profesor

--- user.js
+++ user.js
@@ -6,7 +6,7 @@ const app = express()
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '123',
+    password: '',
     database: 'db_asesorias'
 })
 const INSERT_USER = "INSERT INTO USUARIO (nombre, correo, password) values ('raul', 'raul@gmail.com', '123')";
@@ -31,14 +31,40 @@ app.get('/usuarios', (req,res) => {
     //res.send('go to the users')
 })
 app.get('/usuario/add', (req, res) => {
-    const { nombre, correo, password } = req.query
-    const INSERT = `INSERT INTO USUARIO (nombre, correo, password) values ('${nombre}', '${correo}', '${password}')`
+    const { nombre, correo,telefono, password } = req.query
+    const INSERT = `INSERT INTO USUARIO (nombre,telefono, correo, password) values ('${nombre}', '${telefono}' , '${correo}', '${password}')`
     connection.query(INSERT, (err, results) => {
         if(err) {
             return res.send(err)
         }else {
             return res.send('Usuario agregado')
         }
+    })
+})
+//login usuario
+app.get('/usuario/login', (req, res) => {
+    const { correo, password } = req.query
+    const LOGIN = `SELECT * FROM USUARIO WHERE correo='${correo}' and password='${password}'`
+    connection.query(LOGIN, (err, results) => {
+        if (err) {
+            return res.send(err)
+        }else {
+            return res.json({
+                data: results
+            })
+        } 
+    })
+})
+//Registro profesor
+app.get('/profesor/add', (req, res) => {
+    const { correo, password, nombre, dni } = req.query
+    const REGISTRO_PROFESOR = `INSERT INTO profesor (nombre, dni, correo, password) values ('${nombre}', '${dni}', '${correo}','${password}')`
+    connection.query(REGISTRO_PROFESOR, (err, results) => {
+        if (err) {
+            return res.send(err)
+        }else {
+            return res.send('Profesor agregado')
+        } 
     })
 })
 app.get('/add', (req,res) => {
